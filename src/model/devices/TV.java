ebe1bd47120debf8d55b00a6d4ca9494f4fa4255
Rupesh Bharkar <rupesh.r.bharkar@Rupeshs-MacBook-Air.local>
2025-11-17 21:58:47 +0530

    Complete TV class and EntertainmentControlable Interface

--- src/model/devices/TV.java
+++ src/model/devices/TV.java
@@ -1,5 +1,90 @@
 package model.devices;
 
-public class TV {
+import model.interfaces.EntertainmentControlable;
 
+public class TV extends Device implements EntertainmentControlable {
+	
+	int volumeLevel;
+	int channel;
+	boolean isMuted;
+	
+	public TV(String name) {
+		super(name);
+		this.volumeLevel = 5;
+		this.channel = 1;
+		this.isMuted = false;
+	}
+
+	public TV(String name, int volumeLevel, int channel, boolean isMuted) {
+		super(name);
+		this.volumeLevel = volumeLevel;
+		this.channel = channel;
+		this.isMuted = isMuted;
+	}
+
+	public int getVolumeLevel() {
+		return volumeLevel;
+	}
+
+	public void setVolumeLevel(int volumeLevel) {
+		this.volumeLevel = volumeLevel;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+
+	public boolean isMuted() {
+		return isMuted;
+	}
+
+	public void setMuted(boolean isMuted) {
+		this.isMuted = isMuted;
+	}
+
+	@Override
+	public void volumeUp() {
+		if (isMuted) {
+			isMuted = false;
+		}
+		if (volumeLevel < 100) {
+			volumeLevel++;
+		}
+		System.out.println("Volume Increased to " + volumeLevel);
+		
+	}
+
+	@Override
+	public void volumeDown() {
+		if (volumeLevel > 0) {
+			volumeLevel--;
+		}
+		System.out.println("Volume decreased to " + volumeLevel);
+		
+	}
+
+	@Override
+	public void nextChannel() {
+		channel++;
+		System.out.println("Channel change to " + channel);
+		
+	}
+
+	@Override
+	public void previousChannel() {
+		channel--;
+		System.out.println("Channel change to " + channel);
+		
+	}
+
+	@Override
+	public void switchChannel(int newchannel) {
+		
+		
+	}
+	
 }
--- src/model/interfaces/EntertainmentControlable.java
+++ src/model/interfaces/EntertainmentControlable.java
@@ -1,5 +1,9 @@
 package model.interfaces;
 
 public interface EntertainmentControlable {
-
+	void volumeUp();
+    void volumeDown();
+    void nextChannel();
+    void previousChannel();
+    void switchChannel(int channel);
 }
